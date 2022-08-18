36a233bf454e1e6320eeaf1bba46a7d1f46028e1
Will <doggy.huang@gmail.com>
2022-08-18 13:03:16 +0800

    基本路由設定與路由連結

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: '', component: DashboardComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
