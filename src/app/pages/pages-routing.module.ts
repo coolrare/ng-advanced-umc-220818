a7f10ad79aa0991351fca5d493cc455ae0691863
Will <doggy.huang@gmail.com>
2022-08-18 16:06:27 +0800

    實現延遲載入

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './blank/blank.component';
 import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
-  { path: '', component: PagesComponent }
+  { path: '', component: PagesComponent },
+  { path: 'blank', component: BlankComponent },
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
+import { BlankComponent } from './blank/blank.component';
 
 
 @NgModule({
   declarations: [
-    PagesComponent
+    PagesComponent,
+    BlankComponent
   ],
   imports: [
     CommonModule,
